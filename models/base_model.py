10f98143071458ece41110581795a4d011cdc462
Orino1 <orinomohamed@gmail.com>
2023-08-12 21:08:16 +0100

    Update: Adding the functinality of reconstructing an object from a dictionary + updating the doc string of init method

--- models/base_model.py
+++ models/base_model.py
@@ -18,23 +18,43 @@ class BaseModel:
         updated_at (datetime.datetime object): Update timestamp.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initialize a new BaseModel instance.
-            Sets id to a random uuid4.
-            Sets created_at/updated_at to the current date and time.
+            If a dictionary of a basemodel is passed as **kwarg:
+              1.Sets all atributes from the dictionary **kwargs
+               other than "__class__" key.
+              2.Convert created_at/updated_at to datetime objects
+               before setting them as attributes.
+            No **kwarg passed:
+              1.Sets id to a random uuid4.
+              2.Sets created_at/updated_at to the current date and time.
 
         No_return value.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        # From a single line to 3 due to pip8 80 char rule lol
+                        iso_format = "%Y-%m-%dT%H:%M:%S.%f"
+                        # dt_obj means date object
+                        # Note_to_self: obj.key => assiging something to the
+                        # attribute "key".
+                        dt_obj = datetime.datetime.strptime(value, iso_format)
+                        setattr(self, key, dt_obj)
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """
         Return a string representation of the object.
 
-            Format: [className] (id) __dict__
+        Return (str): With format: "[className] (id) __dict__"
         """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
@@ -50,7 +70,7 @@ class BaseModel:
         """
         Convert the object to a dictionary.
 
-        Returns:
+        Returns (Dictionary):
             dict: dictionary representation of the object with the class name
                   and created_at/updated_at attributes as an isoformat.
         """
