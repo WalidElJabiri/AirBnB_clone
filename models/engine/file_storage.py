10280953e75d6186702f409b780f82d6078bbbf5
Orino1 <orinomohamed@gmail.com>
2023-08-13 19:10:55 +0100

    Update: Calling class attributes with the class name instead of self + adding encoding to open

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -42,7 +42,7 @@ class FileStorage:
         No_return_value.
         """
         key = f"{obj.__class__.__name__}.{obj.id}"
-        self.__objects[key] = obj.to_dict()
+        FileStorage.__objects[key] = obj.to_dict()
 
     def save(self):
         """
@@ -50,7 +50,7 @@ class FileStorage:
 
         No_return value.
         """
-        with open(self.__file_path, "w") as json_file:
+        with open(self.__file_path, "w", encoding="utf-8") as json_file:
             json.dump(self.__objects, json_file)
 
     def reload(self):
@@ -61,5 +61,5 @@ class FileStorage:
         No_return_value.
         """
         if os.path.exists(self.__file_path):
-            with open(self.__file_path, "r") as json_file:
+            with open(self.__file_path, "r", encoding="utf-8") as json_file:
                 FileStorage.__objects = json.load(json_file)
